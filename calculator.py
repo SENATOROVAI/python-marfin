0d72a1ef1d44d7e58c6a406144201dd9aa4b8a45
SergeyMarfin <119563862+SergeyMarfin@users.noreply.github.com>
2023-05-11 09:09:08 +0500

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,19 @@
+def calculator(num, num2, operator):
+    if operator == '+':
+        print('Сложение', num + num2)
+    elif operator == '-':
+        print('Вычитание', num - num2)
+    elif operator == '*':
+        print('Умножение', num * num2)
+    elif operator == '/':
+        print('Деление', num / num2)
+    elif operator == '%':
+        print('Деление по модулю', num % num2)
+
+
+while True:  # До : загаловок, а после инструкция
+
+    num = float(input('Введите число: '))
+    operator = input('Введите операцию: ')
+    num2 = float(input('Введите число: '))
+    calculator(num, num2, operator)
