251b4fa09a49473fd0d0eaa64df68b068d87a7db
SergeyMarfin <119563862+SergeyMarfin@users.noreply.github.com>
2023-05-15 15:22:39 +0500

    changes

--- algorithms/hw_1-cf.py
+++
@@ -1,8 +0,0 @@
-#  вставь ссылку на задачу и перепиши этот файл на юпитер
-num = int(input())
-for i in range(num):
-    word = input()
-    if len(word) > 10:
-        print(word[0] + str(len(word) - 2) + word[-1])
-    else:
-        print(word)
